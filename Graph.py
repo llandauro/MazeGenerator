a4c3c4df94738ba7ca7123677f5081f377022249
llandauro <lara.landauro@gmail.com>
2024-05-16 16:41:08 -0400

    maze generator not working

--- Graph.py
+++ Graph.py
@@ -1,4 +1,4 @@
-class GraphGenerator:
+class Graph:
     class Edge:
         def __init__(self, first_cell, second_cell):
             self.nodes = [first_cell, second_cell]  # nodes connected by this edge
@@ -10,6 +10,8 @@ class GraphGenerator:
     def __init__(self, width, length):
         self.cells = []
         self.edges = []
+        self.width = width
+        self.length = length
         for x_coord in range(width):
             for y_coord in range(length):
                 cell_coordinates = [x_coord, y_coord]
@@ -22,12 +24,31 @@ class GraphGenerator:
                     self.edges.append(self.Edge(self.get_cell_index(x_coord, y_coord),
                                                  self.get_cell_index(x_coord, y_coord + 1)))
 
-    def get_cell_index(self, x_coord, y_coord):
-        return y_coord * width + x_coord
+    def get_cell_index(self, x_coord, y_coord): #maybe no need for width? 
+        return y_coord * self.width + x_coord
+    
+    def get_cell(self, x_coord, y_coord):
+        return self.cells[x_coord][y_coord]
+    
+    def adjacency_list(self):
+        adjacency = {}
+        for cell_index, cell in enumerate(self.cells):
+            adjacency[cell_index] = []
+            for edge in self.edges:
+                if cell_index in edge.nodes:
+                    neighbor = edge.nodes[0] if edge.nodes[1] == cell_index else edge.nodes[1]
+                    adjacency[cell_index].append(neighbor)
+        return adjacency
+    
+    #supposed to enumerate the cells and return the adjacency list related to the cells, and if asked about the current cells,
+    #gives the according neighbors
 
-# test:
-# width = 5
-# length = 5
-# graph = GraphGenerator(width, length)
 
+# # Test
+# width = 2
+# length = 2
+# graph = Graph(width, length)
+# adjacency = graph.adjacency_list()
+# for cell_index, neighbors in adjacency.items():
+#     print(f"Cell {cell_index} has neighbors: {neighbors}")
 
--- MazeGenerator.py
+++ MazeGenerator.py
@@ -1,8 +1,82 @@
-class MazeGenerator:
+import random
+from Graph import Graph
+
+class Maze:
 
     
     # first step is to generate a graph similar to a chess board:
     # We have cell
     # start of the algorithm:
-    # initialize the maze with one cell chosen randomly
+    # initialize the maze with one cell chosen arbitrarily (say cell at coord (0,0)) and 
+    # add it to the UST (Uniform Spanning Tree)
+    # random walk until you reach a cell already in the maze
+
     
+    def __init__(self, graph):
+        self.graph = graph
+        self.maze = set() #maze is a set of edges since we are looking for a subgraph of graphs
+        self.visited = []
+
+    def cell_in_maze(self, cell):
+        result = False
+        #iterate through the maze
+        for edge in self.maze:
+            if edge[0] == cell or edge[1] == cell:
+                result = True
+                break
+        return result
+    
+    def random_walk(self, start_cell):
+        path = []
+        current_cell = start_cell
+        while current_cell not in self.visited:
+            self.visited.append(current_cell)
+            neighbors = self.graph.adjacency_list()[current_cell]
+            next_cell = random.choice(neighbors)
+            path.append((current_cell, next_cell))
+            current_cell = next_cell
+        return path
+
+    def generate_maze(self):
+        #Step 1: Choose a random cell and add it to the set of visited cells
+        random_cell = random.choice(self.graph.cells) #here you get the random cell
+        #keep on selecting random vertices until all are part of the maze
+        #I need to figure out a way to see if a vertex is in the graph by traversing the edges of the graph
+        self.visited.append(random_cell)
+
+        #Step 2: Select a different random cell that is NOT in the visited cells set
+        current_cell = random.choice(self.graph.cells)
+        while current_cell in self.visited:
+            current_cell = random.choice(self.graph.cells)
+
+        while len(self.visited) < len(self.graph.cells):
+            path = self.random_walk(current_cell)
+            self.maze.update(path)
+
+            # Step 3: Select a random cell that has already been visited
+            current_cell = random.choice(list(self.visited))
+
+        return self.maze
+    
+# Test
+width = 3
+length = 3
+graph = Graph(width, length)
+maze_gen = Maze(graph)
+maze = maze_gen.generate_maze()
+
+print("Generated Maze:")
+for edge in maze:
+    print(edge)
+
+    
+            
+
+
+
+
+        
+
+
+
+
